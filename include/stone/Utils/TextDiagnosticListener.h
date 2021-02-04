8b2eb7cf09e7d250aaa75739f1cca09c643dd4ee
McCorney Severin <mccorney.severin@gmail.com>
2021-02-04 14:34:06 -0800

    Added DirScanner.

--- include/stone/Utils/TextDiagnosticListener.h
+++ include/stone/Utils/TextDiagnosticListener.h
@@ -1,9 +1,12 @@
 #ifndef STONE_UTILS_TEXTDIAGNOSTICLISTENER_H
 #define STONE_UTILS_TEXTDIAGNOSTICLISTENER_H
 
-namespace stone {
+#include "stone/Utils/Diagnostics.h"
 
-class TexDiagnosticListener final {};
+namespace stone {
+class TexDiagnosticListener final : public DiagnosticListener {
+ public:
+};
 }  // namespace stone
 
 #endif
