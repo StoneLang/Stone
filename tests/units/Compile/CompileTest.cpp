06ba847b9c766e570b7a9f382c71064b3895e6da
McCorney Severin <mccorney.severin@gmail.com>
2021-06-28 15:03:50 -0700

    Updating tools.

---
+++ tests/units/Compile/CompileTest.cpp
@@ -0,0 +1,17 @@
+
+#include "stone/Basic/LangOptions.h"
+#include "stone/Basic/Mem.h"
+#include "stone/Basic/SrcMgr.h"
+
+#include "gtest/gtest.h"
+
+using namespace stone;
+using namespace stone::syn; 
+
+class CompileTest : public ::testing::Test {
+protected:
+	Basic basic; 
+};
+
+TEST_F(CompileTest, CompileTest) {
+}
